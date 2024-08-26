16d67b3adbbf959446f1e358d44bd9a11feb3011
Ung Huy <ungquanghuy@gmail.com>
2024-08-26 14:24:57 +0700

    add banner element

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -9,3 +9,33 @@ $(".menu .menu__link").on("click", function () {
   $(".nav-toggle").removeClass("open");
   $(".menu").removeClass("collapse");
 });
+
+// Swiper Slider
+// eslint-disable-next-line no-unused-vars, no-undef
+const swiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  effect: "fade",
+  speed: 1000,
+  loop: true,
+
+  // pagination slider
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    }
+  },
+
+  // navigation arrows slider
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  mousewheel: {
+    invert: false,
+    forceToAxis: false,
+    thresholdDelta: 50,
+    sensitivity: 1
+  }
+});
